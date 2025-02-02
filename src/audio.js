e9f704705373190773ed50e6a51ec2d3ec18b5f1
Mark Enty <mark.enty@gmail.com>
2025-02-02 12:41:00 -0800

    Update audio.js

--- src/audio.js
+++ src/audio.js
@@ -1,17 +1,17 @@
 // audio.js -- sounds (both audio and music) for the game
-
-// Copyright (C) 2019, Nicholas Carlini <nicholas@carlini.com>.
 //
+// Copyright (C) 2019, Nicholas Carlini <nicholas@carlini.com>.
+// 
 // This program is free software: you can redistribute it and/or modify
 // it under the terms of the GNU General Public License as published by
 // the Free Software Foundation, either version 3 of the License, or
 // (at your option) any later version.
-//
+// 
 // This program is distributed in the hope that it will be useful,
 // but WITHOUT ANY WARRANTY; without even the implied warranty of
 // MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 // GNU General Public License for more details.
-//
+// 
 // You should have received a copy of the GNU General Public License
 // along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
@@ -19,31 +19,31 @@ var sounds = {}
 
 var context = new AudioContext();
 var play = (which, location) => {
-    var m = context.createBuffer(1,which.length,48e3);
+    var m = context.createBuffer(1, which.length, 48e3);
     m.copyToChannel(new Float32Array(which), 0, 0);
     var src = context.createBufferSource();
     var panner = context.createStereoPanner();
     src.buffer = m;
     src.connect(panner);
-    panner.pan.value = location ? Math.cos(angle_between(location, camera.position) + camera.theta + Math.PI/2) : 0;;
-    panner.connect(context.destination)
+    panner.pan.value = location ? Math.cos(angle_between(location, camera.position) + camera.theta + Math.PI/2) : 0;
+    panner.connect(context.destination);
     src.start(); // QQ
     return src;
 };
 
 var musicnotes;
-var music_timeouts=[];
+var music_timeouts = [];
 function doplay() {
     if (music_timeouts.length < 2 && !going_back && can_play_music) {
-        musicnotes.map((note,i) =>
-                       note.map(n=>music_timeouts.push(setTimeout(_ => {
-                           music_timeouts.shift();
-                           play(n, NewVector(20,-40,0));
-                           if (music_timeouts.length == 1) {
-                               doplay();
-                           }
-                       }, i*200+400)))
-                      )
+        musicnotes.map((note, i) =>
+            note.map(n => music_timeouts.push(setTimeout(_ => {
+                music_timeouts.shift();
+                play(n, NewVector(20, -40, 0));
+                if (music_timeouts.length == 1) {
+                    doplay();
+                }
+            }, i * 200 + 400)))
+        );
     }
 }
 
@@ -131,12 +131,10 @@ function setup_audio() {
            0,5,5,,45,5,,-10,,,,,,,,,-25,,5,,,,,300];
     arr = reshape(arr.map(function(x) { return x / 100; }), 24);
 
-
-
     sounds.boom = toaudio([jsfxr(arr[0])]);
-    sounds.gun = toaudio([jsfxr(arr[1]),jsfxr(arr[2])]);
-    sounds.collect  = toaudio([jsfxr(arr[3])])
-    sounds.collect2 = toaudio([jsfxr(arr[4])])
-    sounds.clock = toaudio([jsfxr(arr[5]), jsfxr(arr[5]).slice(2000)])
-    sounds.hit = toaudio([jsfxr(arr[6]),jsfxr(arr[1])])
+    sounds.gun = toaudio([jsfxr(arr[1]), jsfxr(arr[2])]);
+    sounds.collect  = toaudio([jsfxr(arr[3])]);
+    sounds.collect2 = toaudio([jsfxr(arr[4])]);
+    sounds.clock = toaudio([jsfxr(arr[5]), jsfxr(arr[5]).slice(2000)]);
+    sounds.hit = toaudio([jsfxr(arr[6]), jsfxr(arr[1])]);
 }
